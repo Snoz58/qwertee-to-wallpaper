68fb4837241c5043d43d2173fdc768b246b37c9d
Arsène Thieffry <arsene.thieffry@openmailbox.org>
2017-02-13 13:32:56 +0100

    Améliorations diverses

---
+++ template.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Qwertee to Wallpaper</title>
+		<!-- Google Fonts -->
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<!-- CSS Reset -->
+		<link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
+		<!-- Milligram CSS minified -->
+		<link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
+		<!-- Custom CSS -->
+		<link rel="stylesheet" type="text/css" href="./style.css">
+	</head>
+
+ 	<body>
+        <div class="container">
+			<div class="row">
+				<div class="column column-50 column-offset-25">
+					<a href="./index.php"><h1 class="title">Qwertee to Wallpaper</h1></a>
+					<p class="description">
+						Création de fond d'écran à partir d'images de <a href="https://www.qwertee.com/shop">Qwertee</a>.
+					</p>
+					<form action="index.php" method="post">
+						<fieldset>
+							<label for="url">Url de la source</label>
+							<input type="text" placeholder="https://www.qwertee.com/product/..." id="url" name="url" value="<?= $url ?>">
+
+							<input type="checkbox" id="recadrer" name="recadrer">
+							<label class="label-inline" for="recadrer">Recadrer l'image (Provoque une perte de qualité)</label>
+
+							<label for="screenx">Résolution souhaitée</label>
+							<input type="number" value="" id="screenw" name="screenw">
+							<input type="number" value="" id="screenh" name="screenh">
+
+							<input class="button-primary" type="submit" value="Générer" name="generer">
+						</fieldset>
+					</form>
+<?php if (isset($_POST["generer"])) : ?>
+					<div class="image">
+						<img src="<?php echo $imageMini; ?>">
+						<div class="clearfix"></div>
+						<a download="qwertee_wallpaper.jpg" href="<?= $path ?>"><button class="button-primary">Télécharger</button></a>
+						<a href="<?= $path ?>" target="_blank"><button class="button-primary">Afficher</button></a>
+					</div>
+<?php endif; ?>
+				</div>
+			</div>
+
+		</div>
+
+        <footer>
+            <div class="float-left">Made with <3 by Corentin MÉTÉNIER - <a href="https://github.com/Snoz58/qwertee-to-wallpaper">GitHub</a></div>
+            <div class="float-right">CSS : Milligram - <a href="https://github.com/milligram/milligram">GitHub</a></div>
+        </footer>
+
+		<script type="text/javascript">
+			var screenw = document.getElementById('screenw').value = screen.width;
+			var screenh = document.getElementById('screenh').value = screen.height;
+		</script>
+
+    </body>
+
+</html>
--- wallpaper.php
+++ wallpaper.php
@@ -1,62 +1,67 @@
-<?php 		
+<?php
 
-header('Content-Type: image/jpeg');
+//$image = $_GET['image'];
 
-$image = $_GET['image'];
+$path = 'images/'.md5($image).'.jpg';
 
-// Récupération de la couleur de fond de l'image source
-$source = imagecreatefromjpeg($image);
-$rgb = imagecolorat($source, 1, 1);
-$colors = imagecolorsforindex($source, $rgb);
+if (!file_exists($path)) {
+	// Récupération de la couleur de fond de l'image source
+	$source = imagecreatefromjpeg($image);
+	$rgb = imagecolorat($source, 1, 1);
+	$colors = imagecolorsforindex($source, $rgb);
 
 
-// Taille de la future image
-$width = 1920;
-$height = 1080;
+	// Taille de la future image
+	$width = 1920;
+	$height = 1080;
 
-// Paramètres de l'image
-$recadrage = $_GET["recadrer"];
-// $recadrage = true;
+	// Paramètres de l'image
+	//$recadrage = $_GET["recadrer"];
+	// $recadrage = true;
 
-$destination = @imagecreatetruecolor($width, $height) or die ("Erreur lors de la création de l'image");
-imagefill ( $destination , 0 , 0 , $rgb );
+	$destination = @imagecreatetruecolor($width, $height) or die ("Erreur lors de la création de l'image");
+	imagefill ( $destination , 0 , 0 , $rgb );
 
-// Partie copiée de la source
-$src_x = 0;
-$src_y = 0;
-$src_w = imagesx($source);
-$src_h = imagesy($source);
+	// Partie copiée de la source
+	$src_x = 0;
+	$src_y = 0;
+	$src_w = imagesx($source);
+	$src_h = imagesy($source);
 
-if ($recadrage){
+	if ($recadrage){
 
-	if ($height < $width){
-		// Redimensionnement par la hauteur 
-		$dst_h = $height;
-		$dst_w = $src_w*$dst_h/$src_h;
-		$dst_x = ($width-$dst_w)/2;
-		$dst_y = ($height-$dst_h)/2;
+		if ($height < $width){
+			// Redimensionnement par la hauteur
+			$dst_h = $height;
+			$dst_w = $src_w*$dst_h/$src_h;
+			$dst_x = ($width-$dst_w)/2;
+			$dst_y = ($height-$dst_h)/2;
+		}
+
+		else{
+			// Redimensionnement par la largeur
+			$dst_w = $width;
+			$dst_h = $src_h*$dst_w/$src_w;
+			$dst_x = ($width-$dst_w)/2;
+			$dst_y = ($height-$dst_h)/2;
+		}
+
 	}
 
 	else{
-		// Redimensionnement par la largeur
-		$dst_w = $width; 
-		$dst_h = $src_h*$dst_w/$src_w;
-		$dst_x = ($width-$dst_w)/2;
-		$dst_y = ($height-$dst_h)/2;
+		// Pas de redimensionnement
+		$dst_x = ($width-$src_w)/2;
+		$dst_y = ($height-$src_h)/2;
+		$dst_w = $src_w;
+		$dst_h = $src_h;
 	}
 
+	imagecopyresized($destination, $source, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h);
+
+
+
+	imagejpeg($destination, $path, 100);
 }
+//header('Location: http://localhost/druz/qwertee-to-wallpaper/'.$path);
 
-else{
-	// Pas de redimensionnement
-	$dst_x = ($width-$src_w)/2;
-	$dst_y = ($height-$src_h)/2;
-	$dst_w = $src_w;
-	$dst_h = $src_h;
-}
-
-imagecopyresized($destination, $source, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h);
-
-imagejpeg($destination, null, 100);
-
- ?>
+?>
